214232cb2c30c505db3708c691b6e93850fce23f
0xpsicod0s <yurisilva7840@gmail.com>
2024-07-29 01:32:35 -0300

    just debugging

--- middlewares/frontMiddleware.js
+++ middlewares/frontMiddleware.js
@@ -4,22 +4,32 @@ import { restrictedTokens } from '../controllers/authController.js';
 export const frontAuthentication = (req, res, next) => {
     req.accessGranted = false;
     const tokenHeader = req.headers.cookie;
+    console.log(`Cookie header: ${tokenHeader}`);
     if (!tokenHeader) return next();
     const authHeader = `Bearer ${tokenHeader.split(';')[0].split('=')[1]}`;
+    console.log(`Token formatado: ${authHeader}`);
     if (!authHeader) return next();
 
     const findToken = restrictedTokens.find(token => token === authHeader);
     if (findToken) {
+        console.log(`Token no acesso restrito: ${findToken}`);
         res.clearCookie('token', { path: '/' });
         return next();
     };
     const parts = authHeader.split(' ');
+    console.log(`Quantidade de partes dividas do token (deve ser 2): ${parts}`);
     if (!parts.length === 2) return next();
     
     const [ scheme, token ] = parts;
-    if (!/^Bearer$/i.test(scheme)) return next();
+    if (!/^Bearer$/i.test(scheme)) {
+        console.log(`Palavra enviada diferente de Bearer: ${scheme}`);
+        return next();
+    }
     jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-        if (err) return next();
+        if (err) {
+            console.log(`Houve um erro ao realizar a autenticidade do token: ${err}`);
+            return next();
+        }
         req.userId = decoded.id;
         req.formattedToken = authHeader;
         req.accessGranted = true;
@@ -28,7 +38,10 @@ export const frontAuthentication = (req, res, next) => {
 }
 
 export const accessNotGranted = (req, res, next) => {
-    if (!req.accessGranted) return res.redirect('/');
+    if (!req.accessGranted) {
+        console.log('Acesso nao garantido');
+        return res.redirect('/');
+    }
     next();
 }
 
