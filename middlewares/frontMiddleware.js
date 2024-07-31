12c17467c6866454bfd7b8d675689a87d5ef2e65
0xpsicod0s <yurisilva7840@gmail.com>
2024-07-31 00:04:53 -0300

    bug finally solved (I hope XD)

--- middlewares/frontMiddleware.js
+++ middlewares/frontMiddleware.js
@@ -8,31 +8,25 @@ export const frontAuthentication = (req, res, next) => {
 
     const cookies = cookieHeader.split(';');
     const tokenHeader = cookies.find(cookie => cookie.startsWith(' token='));
-    console.log('token: ', tokenHeader);
     if (!tokenHeader) return next();
 
     const authHeader = `Bearer ${tokenHeader.split('=')[1]}`;
-    console.log('token mal formatado: ', authHeader);
     if (!authHeader) return next();
 
     const findToken = restrictedTokens.find(token => token === authHeader);
     if (findToken) {
-        console.log('token restrito');
         res.clearCookie('token', { path: '/' });
         return next();
     };
     const parts = authHeader.split(' ');
-    console.log(parts.length);
     if (!parts.length === 2) return next();
     
     const [ scheme, token ] = parts;
-    console.log(scheme, token);
     if (!/^Bearer$/i.test(scheme)) {
         return next();
     }
     jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
         if (err) {
-            console.log('deu ruim');
             return next();
         }
         req.userId = decoded.id;
@@ -44,7 +38,6 @@ export const frontAuthentication = (req, res, next) => {
 
 export const accessNotGranted = (req, res, next) => {
     if (!req.accessGranted) {
-        console.log('caiu aq');
         return res.redirect('/');
     }
     next();
